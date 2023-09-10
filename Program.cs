603d3249b443c6a2c564938882fe0d330934f89c
Bietood <gabriel.duarte69@etec.sp.gov.br>
2023-09-10 19:25:40 -0300

    exercício resolvido

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+string Nome1, Nome2, Auxiliar;
+
+Console.WriteLine("--- Troca de Valores ---\n");
+
+Console.Write("Nome 1..: ");
+Nome1 = Console.ReadLine()!;
+
+Console.Write("Nome 2..: ");
+Nome2 = Console.ReadLine()!;
+
+Auxiliar = Nome1;
+Nome1 = Nome2;
+Nome2 = Auxiliar;
+
+Console.WriteLine("\nApós trocar os valores temos:\n");
+
+Console.WriteLine($"Nome 1 = {Nome1}");
+Console.WriteLine($"Nome 2 = {Nome2}");
